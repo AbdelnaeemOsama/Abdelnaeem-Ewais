5204c2cd314915d8a9601703798257d35a758151
AbdelnaeemOsama <125660203+AbdelnaeemOsama@users.noreply.github.com>
2024-08-20 16:16:56 +0300

    Add files via upload

---
+++ TASK/task.js
@@ -0,0 +1,36 @@
+/*let x,y,z;
+x = 3;
+y = 10; 
+
+let user = {name : "Abdelnaeem", age : "20", Date : "20-8-2024"};
+
+  new student = new Array (10, 20, 30,40);
+ typeof(user);
+ typeof(student);
+
+x= x + user;
+
+console.log(x);
+
+consosle.log(user.name );
+*/
+let x =10;
+console.log (x)
+function hellox(){
+    let x=20;
+    console.log (x);
+
+}
+hellox();
+console.log (x);
+
+
+var z=10;
+console.log(y);
+function helloz(){
+    var z=20;
+    console.log(z);
+
+}
+helloz();
+console.log (z);
